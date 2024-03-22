2861f42e92c9f4c008e1e28845a6f8b3e84ac768
Logon PF Fiap <logonpflocal@fiap.com.br>
2024-03-22 20:37:58 -0300

    Comentario corrigido

--- script.js
+++ script.js
@@ -106,7 +106,7 @@ btn1.addEventListener('click',()=>{
     inputUsuario.value = ''
 })
 /*
-Função que é executada quando o botão de favorito é clicado
+Função corrigaaaaaa
 */
 
 const favoritoClicado = (eventoDeClique) => {
